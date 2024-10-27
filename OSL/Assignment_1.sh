53ebfb43fa5d19f09e5a2bcd3732989ebdc4b7cf
rohan3066-dev <rohan.chavan@innokarma.com>
2024-10-27 11:46:47 +0530

    Assignment_1

--- OSL/Assignment_1.sh
+++ OSL/Assignment_1.sh
@@ -81,6 +81,102 @@ insert()
 
 }
 
+modify()
+{
+    echo "Enter address book name"
+
+    read ab
+
+    #check address book is available or not in current directory
+
+    res=` ls | grep $ab | wc -w`
+
+    if [ $res -gt 0 ]
+    then
+        echo "Enter your email"
+        read email
+
+        len=`cat $ab | grep $email | wc -w`
+        if [ $len -gt 0 ]
+        then    
+
+            echo "Enter Modified first name,last name,mobile No"
+
+            read fname lname mobile
+            new=`echo $fname $lname $email $mobile`
+
+            old=`cat $ab | grep $email`
+
+            echo "Old record : $old"
+            echo "New record : $new"
+
+            sed -i s/"$old"/"$new"/g $ab
+
+            echo "record is modified!!"
+
+
+            
+
+        else
+
+
+            echo "Error email not is exiting!!"
+
+        fi
+
+    else
+       echo "Error file is not exit!!"
+
+    fi
+
+
+}
+
+delete()
+{
+    echo "Enter address book name"
+
+    read ab
+
+    #check address book is available or not in current directory
+
+    res=` ls | grep $ab | wc -w`
+
+    if [ $res -gt 0 ]
+    then
+        echo "Enter your email"
+        read email
+
+        len=`cat $ab | grep $email | wc -w`
+        if [ $len -gt 0 ]
+        then    
+
+            old=`cat $ab | grep $email`
+
+            sed -i s/"$old"//g $ab
+
+            sed -i /^$/d $ab
+
+            echo "record is Deleted!!"
+
+
+            
+
+        else
+
+
+            echo "Error email not is exiting!!"
+
+        fi
+
+    else
+       echo "Error file is not exit!!"
+
+    fi
+
+
+}
+
 while [ true ]
 
 do 
@@ -88,7 +184,9 @@ do
     echo "1.Create Address Book"
     echo "2.Display Address Book"
     echo "3.Insert Book record"
-    echo "7.Exit"
+    echo "4.Modify Book record"
+    echo "5.Delete Book record"
+    echo "6.Exit"
     echo "*********************************"
 
     echo "Enter choice"
@@ -100,7 +198,9 @@ do
         1) create ;;
         2)display ;;
         3)insert ;;
-        7)exit ;;
+        4)modify ;;
+        5)delete ;;
+        6)exit ;;
         *) echo "Wrong choice!!"
 
     esac
