38005c1db6ed4e95c6dcf6a0877dbdda64694f9c
rohan3066-dev <rohan.chavan@innokarma.com>
2024-10-30 16:29:05 +0530

    changes done

--- OSL/tempCodeRunnerFile.sh
+++ OSL/tempCodeRunnerFile.sh
@@ -1,5 +1,3 @@
-
-
 create()
 {
     echo "Enter address book name"
@@ -8,7 +6,7 @@ create()
 
     #check address book is available or not in current directory
 
-    res=` ls | grep &ab | wc -w`
+    res=` ls | grep $ab | wc -w`
 
     if [ $res -gt 0 ]
     then
@@ -22,13 +20,173 @@ create()
     fi
 }
 
+display()
+{
+    echo "Enter address book name"
+
+    read ab
+
+    #check address book is available or not in current directory
+
+    res=` ls | grep $ab | wc -w`
+
+    if [ $res -gt 0 ]
+    then
+        cat $ab
+
+    else
+       
+      echo "Error : File is not exiting!!" 
+
+    fi
+
+}
+
+
+insert()
+{
+    echo "Enter address book name"
+
+    read ab
+
+    #check address book is available or not in current directory
+
+    res=` ls | grep $ab | wc -w`
+
+    if [ $res -gt 0 ]
+    then
+        echo "Enter your email"
+        read email
+
+        len=`cat $ab | grep $email | wc -w`
+        if [ $len -gt 0 ]
+        then    
+            echo "Error: email already present"
+
+        else
+            echo "Enter first name,last name,mobile no"
+            read fname lname mobile 
+
+            record=`echo $fname $lname $email $mobile`
+
+            echo $record >> $ab
+            echo "record inserted!!"
+        fi
+
+    else
+       echo "Error file is not exit!!"
+
+    fi
+
+
+}
+
+modify()
+{
+    echo "Enter address book name"
+
+    read ab
+
+    #check address book is available or not in current directory
+
+    res=` ls | grep $ab | wc -w`
+
+    if [ $res -gt 0 ]
+    then
+        echo "Enter your email"
+        read email
+
+        len=`cat $ab | grep $email | wc -w`
+        if [ $len -gt 0 ]
+        then    
+
+            echo "Enter Modified first name,last name,mobile No"
+
+            read fname lname mobile
+            new=`echo $fname $lname $email $mobile`
+
+            old=`cat $ab | grep $email`
+
+            echo "Old record : $old"
+            echo "New record : $new"
+
+            sed -i s/"$old"/"$new"/g $ab
+
+            echo "record is modified!!"
+
+
+            
+
+        else
+
+
+            echo "Error email not is exiting!!"
+
+        fi
+
+    else
+       echo "Error file is not exit!!"
+
+    fi
+
+
+}
+
+delete()
+{
+    echo "Enter address book name"
+
+    read ab
+
+    #check address book is available or not in current directory
+
+    res=` ls | grep $ab | wc -w`
+
+    if [ $res -gt 0 ]
+    then
+        echo "Enter your email"
+        read email
+
+        len=`cat $ab | grep $email | wc -w`
+        if [ $len -gt 0 ]
+        then    
+
+            old=`cat $ab | grep $email`
+
+            sed -i s/"$old"//g $ab
+
+            sed -i /^$/d $ab
+
+            echo "record is Deleted!!"
+
+
+            
+
+        else
+
+
+            echo "Error email not is exiting!!"
+
+        fi
+
+    else
+       echo "Error file is not exit!!"
+
+    fi
+
+
+}
+
 while [ true ]
 
 do 
     echo "***********MENU*****************"
     echo "1.Create Address Book"
     echo "2.Display Address Book"
-    echo "7.Exit"
+    echo "3.Insert Book record"
+    echo "4.Modify Book record"
+    echo "5.Delete Book record"
+    echo "6.Exit"
     echo "*********************************"
 
     echo "Enter choice"
@@ -38,7 +196,11 @@ do
     case $choice in 
 
         1) create ;;
-        2)exit ;;
+        2)display ;;
+        3)insert ;;
+        4)modify ;;
+        5)delete ;;
+        6)exit ;;
         *) echo "Wrong choice!!"
 
     esac
